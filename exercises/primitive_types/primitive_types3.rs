f4d4226c66395250d92104e3a30243689b40d632
Huan Min Gan <17909473+ministrudels@users.noreply.github.com>
2023-04-12 21:52:51 +0100

    primitive_types, vecs, move_semantics, structs, enums, strings, modules, hashmaps, quiz2

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,12 +2,16 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use core::ops::Range;
 
 fn main() {
-    let a = ???
+    // Create an array of length 100
+    let hundred_zeros: [u8; 100] = [0; 100];
+    let zero_to_hundred: Range<i32> = 0..100;
 
-    if a.len() >= 100 {
+    if hundred_zeros.len() >= 100 && zero_to_hundred.len() >= 100 {
+        println!("{:?}", hundred_zeros);
+        println!("{:?}", zero_to_hundred);
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -29,12 +27,21 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
+
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(u) => {
+                    let mut new_string = string.clone();
+                    for _ in 0..u.clone() {
+                        new_string.push_str("bar");
+                    }
+                    output.push(new_string);
+                }
+            }
         }
         output
     }
@@ -42,8 +49,8 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    // // TODO: What do we need to imp
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
